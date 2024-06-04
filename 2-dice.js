ec0e6df81957f611aa93c521116ebcc23c8aea61
Мария <kolesnikovamaria169@gmail,com>
2024-06-04 22:00:51 +0300

    Второе задание

--- 2-dice.js
+++ 2-dice.js
@@ -1,8 +1,13 @@
-const array = { 4: "D4", 6: 'D6', 8: 'D8', 10: 'D10', 12: 'D12', 16: 'D16', 20: 'D20' };
-function dice(array) {
-    Object.keys(array).forEach(element => { console.log(Math.floor(Math.random() * Number(element) + 1)) });
+const array = {
+    D4: 4,
+    D6: 6,
+    D8: 8,
+    D10: 10,
+    D12: 12,
+    D16: 16,
+    D2: 20
 };
-console.log(dice(array));
-
-
-console.log(dice(array));
+function dice(array, key) {
+    return (Math.floor(Math.random() * Number(array[key]) + 1));
+};
+console.log(dice(array, 'D12'));
